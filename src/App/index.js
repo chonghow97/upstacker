fdf72e4c7f05996ce6e42a26be2bacd88a6788eb
chonghow97 <31228115+chonghow97@users.noreply.github.com>
2022-03-28 22:16:27 +0800

    feat: install sentry (#10)

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import * as Sentry from '@sentry/react-native';
+import {DSN as dsn} from '@env';
 import Provider from './Provider';
 import RootNavigator from '../navigation/RootNavigator';
+
+Sentry.init({
+  dsn,
+  // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+  // We recommend adjusting this value in production.
+  tracesSampleRate: 1.0,
+});
 
 const App = () => (
   <Provider>
@@ -8,4 +17,4 @@ const App = () => (
   </Provider>
 );
 
-export default App;
+export default Sentry.wrap(App);
