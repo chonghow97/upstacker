6caa4209ac5a6cd9d1d5a4abb7d4b87dc507a16b
chonghow97 <31228115+chonghow97@users.noreply.github.com>
2022-03-28 13:07:00 +0800

    feat: onScroll api for screen component (#7)

--- src/components/Screen/index.js
+++ src/components/Screen/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {SafeAreaView, View} from 'react-native';
+import {SafeAreaView, View, FlatList} from 'react-native';
 import {useTailwind} from 'tailwind-rn';
 import classNames from 'classnames';
 
 const isIos = Platform.OS === 'ios';
 
 export const Screen = props => {
-  const {children, className, ...restProps} = props;
+  const {children, className, onScroll, ...restProps} = props;
   const tw = useTailwind();
 
   const myStyle = tw(
@@ -15,7 +15,15 @@ export const Screen = props => {
 
   return (
     <SafeAreaView style={myStyle} {...restProps}>
-      <View style={tw('pt-3 px-1.5')}>{children}</View>
+      {onScroll ? (
+        <FlatList
+          ListHeaderComponent={
+            <View style={tw('pt-3 px-1.5')}>{children}</View>
+          }
+        />
+      ) : (
+        <View style={tw('pt-3 px-1.5')}>{children}</View>
+      )}
     </SafeAreaView>
   );
 };
